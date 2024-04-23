ec94b13c693b5e0a117e186127961aa3141051dd
Visnicio <vinicius.antonio0712@gmail.com>
2024-04-22 23:20:59 -0300

    added alpine and htmx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to HALT</title>
 
+    {{-- HALT's dependencies --}}
     <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/htmx.js')}}"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
+
 </head>
 <body class="bg-blue-950 bg-gradient-to-bl from-blue-950 to-purple-900 text-white min-h-screen flex flex-col justify-center items-center">
     <header class="font-bold text-2xl">Welcome to the HALT stack </header>
@@ -18,5 +23,7 @@
         <li>Laravel</li>
         <li>TailwindCSS</li>
     </ul>
+
+    <button type="button" hx-get="/hello-world" class="mt-10 p-2 text-black rounded-lg bg-white hover:bg-gray-400">Click to change text via HTMX</button>
 </body>
 </html>

e09b128d465d3d20c8543d00fce2fcdf75cbd206
Visnicio <vinicius.antonio0712@gmail.com>
2024-04-22 23:05:57 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to HALT</title>
+
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+</head>
+<body class="bg-blue-950 text-white min-h-screen flex flex-col justify-center items-center">
+    <header class="font-bold text-2xl">Welcome to the HALT stack </header>
+    <div>For the giga chads, no cringe bundlers here.</div>
+
+    <div class="mt-10">Just import what you need.</div>
+    <ul class="list-disc">
+        <li>HTMX</li>
+        <li>AlpineJS</li>
+        <li>Laravel</li>
+        <li>TailwindCSS</li>
+    </ul>
+</body>
+</html>
